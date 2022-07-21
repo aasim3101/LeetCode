00326cc219af555c81563d04e67f9a71d50f0702
Mohd Aasim Qureshi <aasimqureshi31jan@gmail.com>
2022-07-21 12:51:19 +0530

    Time: 2 ms (74.61%), Space: 7.4 MB (93.11%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,32 +10,45 @@
  */
 class Solution {
 public:
-    ListNode* leftPointer = new ListNode();
-    bool stop=false;
-    bool backtrack=false;
-    void helper(int left, int right, ListNode* rightPointer){
-        if(right == 1){
-            return;
-        }
-        rightPointer=rightPointer->next;
-        if(left > 1){
-            leftPointer=leftPointer->next;
-        }
-        helper(left-1,right-1,rightPointer);
-        if(rightPointer == leftPointer || rightPointer->next == leftPointer){
-            stop=true;
-        }
-        if(!stop){
-            int t=leftPointer->val;
-            leftPointer->val=rightPointer->val;
-            rightPointer->val=t;
-            leftPointer=leftPointer->next;
-        }
-    }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        leftPointer = head;
-        ListNode* rightPointer = head;
-        helper(left, right, rightPointer);
+        if(left == right){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* tail = new ListNode();
+        ListNode* con = new ListNode();
+        int cnt = 1;
+        while(1){
+            if(cnt == left){
+                tail = curr;
+                con = prev;
+                prev = curr;
+                curr = curr->next;
+                cnt++;
+                continue;
+            }
+            if(cnt>=left && cnt<=right){
+                ListNode* third = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = third;
+            }
+            if(cnt == right){
+                break;
+            }
+            if(cnt < left || cnt > right){
+                prev = curr;
+                curr = curr->next;
+            }
+            cnt++;
+        }
+        if(con == NULL){
+            head = prev;
+        }
+        if(con != NULL)
+            con->next = prev;
+        tail->next = curr;
         return head;
     }
 };
